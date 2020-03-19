c034b10b5bfb49012927b909b2ba0772016739b7
Miroslav Dranchovski <mikiko16@abv.bg>
2020-03-19 07:20:36 +0200

    Hope Working !

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
+import { TeambuildingComponent } from './teambuilding/teambuilding/teambuilding.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, Route,  RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Route[] = [
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'teambuilding', component: TeambuildingComponent}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
+import { InjectionToken } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import { TokenInterceptor } from './interceptor/interceptor';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
+import { TeambuildingComponent } from './teambuilding/teambuilding/teambuilding.component'
+
+export const BASE_URL = new InjectionToken<string>('BASE_URL');
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavMenuComponent,
+    HomeComponent,
+    TeambuildingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule,
+    FormsModule,
+    CommonModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [{ provide: BASE_URL , useValue: "https://localhost:5001/" },
+  {
+    provide: HTTP_INTERCEPTORS,      
+    useClass: TokenInterceptor,      
+    multi: true   
+    }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
